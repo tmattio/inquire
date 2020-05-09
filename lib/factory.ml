4e1e285d7fcad32656de51ed4498455f64a7a8a0
Thibaut Mattio <thibaut.mattio@gmail.com>
2020-05-09 13:44:47 +0200

    Format

--- lib/factory.ml
+++ lib/factory.ml
@@ -2,9 +2,11 @@ module Make (M : Impl.M) = struct
   let confirm ?default message =
     Confirm.prompt message ?default ~impl:(module M)
 
-  let password ?validate message = Password.prompt message ?validate ~impl:(module M)
+  let password ?validate message =
+    Password.prompt message ?validate ~impl:(module M)
 
-  let input ?validate ?default message = Input.prompt message ?validate ?default ~impl:(module M)
+  let input ?validate ?default message =
+    Input.prompt message ?validate ?default ~impl:(module M)
 
   let raw_select ?default ~options message =
     Raw_select.prompt message ?default ~options ~impl:(module M)
